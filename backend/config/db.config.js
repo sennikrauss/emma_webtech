1e0da9cc5a1f6b1878710d9d5f10dd85f563fefd
sennikrauss <s0568990@htw-berlin.de>
2021-03-05 19:15:12 +0100

    first initialized backend

---
+++ backend/config/db.config.js
@@ -0,0 +1,11 @@
+/**
+ * Schritt 2
+ * studi-server-Datenbank ans backend anbinden
+ * @type {{PASSWORD: string, HOST: string, USER: string, DB: string}}
+ */
+module.exports = {
+    HOST: "db.f4.htw-berlin.de",
+    USER: "s0568990",
+    PASSWORD: "Password",
+    DB: "_s0568990__tanteemma"
+};
---
+++ backend/server.js
@@ -0,0 +1,33 @@
+/**
+ * Schritt 1
+ * zusätzliche Module installieren: npm install express mysql body-parser --save
+ * Module express, mysql und body-parser konfigurieren für backend
+ *
+ * Schritt 6
+ * Routes (artikel.routes.js) einlesen über require
+ * @type {e | (() => Express)}
+ */
+const express = require("express");
+const cors = require('cors');
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// enable cors for all requests
+app.use(cors());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Hello Tante Emma!" });
+});
+
+// set port, listen for requests
+app.listen(3000, () => {
+    console.log("Server is running on port 3000.");
+});
