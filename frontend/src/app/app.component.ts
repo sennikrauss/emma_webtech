4e12eddf36bb3bb3d13aa3f22f17eab4b38bf98d
PMahulome <s0569780@htw-berlin.de>
2021-03-06 18:22:13 +0100

    navbar-responsive:Navigations-komponente lassen sich anzeigen

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
-}
+
+ngOnInit(){}
+  navbarOpen=false;
+
+toggleNavbar(){
+  this.navbarOpen=!this.navbarOpen!;
+                 }
+  }
