ae0f797d773ff7aeb143c2e563bdb6e4310936eb
PMahulome <s0569780@htw-berlin.de>
2021-03-17 17:20:27 +0100

    login navigate to home & add Register button

---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+ getUserDetails(): any {
+    return localStorage.getItem('userData') ? JSON.parse(localStorage.getItem('userData')) : null;
+  }
+
+  setDataInLocalStorage(variableName, data): any {
+    localStorage.setItem(variableName, data);
+  }
+
+  clearStorage(): any {
+    localStorage.clear();
+  }
+
+}
