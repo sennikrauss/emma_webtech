a6044db074104a897718e721048f2ff9db643b3a
sennikrauss <s0568990@htw-berlin.de>
2021-03-28 17:19:41 +0200

    little changes

--- frontend/src/app/user/header/header.component.ts
+++ frontend/src/app/user/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -9,9 +11,15 @@ export class HeaderComponent implements OnInit {
   navbarOpen=false;
 
 
-  constructor() { }
+  constructor(public auth:AuthService, public router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  logout(): void{
+    this.auth.clearStorage();
+    this.router.navigate(['']);
+    location.reload()
   }
 
   toggleNavbar(){
