9b9791b300115f9efc0338c54c7fbfb7b7b16b15
sennikrauss <s0568990@htw-berlin.de>
2022-08-25 08:56:36 +0200

    add files to gitignore

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -3,9 +3,16 @@
  * studi-server-Datenbank ans backend anbinden
  * @type {{PASSWORD: string, HOST: string, USER: string, DB: string}}
  */
-module.exports = {
+/*module.exports = {
     HOST: "db.f4.htw-berlin.de",
     USER: "s0568990",
     PASSWORD: "Password",
     DB: "_s0568990__tanteemma"
+};*/
+
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "Meinach7010",
+    DB: "tante_emma"
 };
